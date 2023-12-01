3f7c9e54dd1d06442ae406d77e11832f80edd492
Bernie Gray <bfgray3@gmail.com>
2023-12-01 17:42:43 -0500

    1a in cpp

--- 01/a/main.cpp
+++ 01/a/main.cpp
@@ -1,5 +1,36 @@
+#include <algorithm>
 #include <iostream>
+#include <numeric>
+#include <string>
+
+#include "utils.h"
 
 int main(const int, const char** argv) {
-  std::cout << TODO << '\n';
+  auto document = utils::read_vector_from_file<std::string>(argv[1]);
+  std::vector<char> first, last;
+
+  std::transform(
+    std::cbegin(document),
+    std::cend(document),
+    std::back_inserter(first),
+    [](const std::string& s) { return *std::find_if(std::cbegin(s), std::cend(s), ::isdigit); }
+  );
+
+  std::transform(
+    std::cbegin(document),
+    std::cend(document),
+    std::back_inserter(last),
+    [](const std::string& s) { return *std::find_if(std::crbegin(s), std::crend(s), ::isdigit); }
+  );
+
+  auto answer = std::transform_reduce(
+    std::cbegin(first),
+    std::cend(first),
+    std::cbegin(last),
+    0ull,
+    std::plus<>(),
+    [](const char a, const char b) { return 10 * (a - '0') + b - '0'; }
+  );
+
+  std::cout << answer << '\n';
 }
