1bdf51cf39bd3c6f1737abedbedbe05a27b4b88c
Bernie Gray <bfgray3@gmail.com>
2023-12-03 21:20:19 -0500

    cleanup

--- 01/a/main.cpp
+++ 01/a/main.cpp
@@ -1,12 +1,16 @@
 #include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <numeric>
 #include <string>
+#include <vector>
 
 #include "utils.h"
 
 int main(const int, const char** argv) {
-  auto document = utils::read_vector_from_file<std::string>(argv[1]);
+  // don't need the whole thing in memory at once, but read it all
+  // so we can use algorithms
+  auto document{utils::read_vector_from_file<std::string>(argv[1])};
   std::vector<char> first, last;
 
   std::transform(
@@ -23,14 +27,14 @@ int main(const int, const char** argv) {
     [](const std::string& s) { return *std::find_if(std::crbegin(s), std::crend(s), ::isdigit); }
   );
 
-  auto answer = std::transform_reduce(
+  auto answer{std::transform_reduce(
     std::cbegin(first),
     std::cend(first),
     std::cbegin(last),
     0ull,
     std::plus<>(),
     [](const char a, const char b) { return 10 * (a - '0') + b - '0'; }
-  );
+  )};
 
   std::cout << answer << '\n';
 }
