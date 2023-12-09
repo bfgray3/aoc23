d4b8edadc404388c43615e6073ea8cc282f62537
Bernie Gray <bfgray3@gmail.com>
2023-12-08 21:21:19 -0500

    polish

--- 03/a/main.py
+++ 03/a/main.py
@@ -4,33 +4,27 @@ import sys
 from collections.abc import Iterable
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True, slots=True)
 class Number:
     row: int
-    num: str
+    num: int
     start: int
     end: int
 
     def is_adjacent_to_symbol(self, symbol_dict: dict[int, list[int]]) -> bool:
+        for x in (self.start - 1, self.end):
+            if self.row in symbol_dict and x in symbol_dict[self.row]:
+                return True
         for r in (self.row - 1, self.row + 1):
             for i in range(self.start - 1, self.end + 1):
-                try:
-                    if i in symbol_dict[r]:
-                        return True
-                except KeyError:
-                    pass
-        for x in (self.start - 1, self.end):
-            try:
-                if x in symbol_dict[self.row]:
+                if r in symbol_dict and i in symbol_dict[r]:
                     return True
-            except KeyError:
-                pass
         return False
 
 
 def find_numbers(x: Iterable[str]) -> list[Number]:
     return [
-        Number(i, m.group(1), *m.span())
+        Number(i, int(m.group(1)), *m.span())
         for i, r in enumerate(x)
         for m in re.finditer(r"(\d+)", r)
     ]
@@ -48,6 +42,4 @@ symbols = {
     i: [j for j, x in enumerate(row) if is_symbol(x)] for i, row in enumerate(schematic)
 }
 
-print(
-    sum(int(n.num) for n in find_numbers(schematic) if n.is_adjacent_to_symbol(symbols))
-)
+print(sum(n.num for n in find_numbers(schematic) if n.is_adjacent_to_symbol(symbols)))
