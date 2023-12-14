29ede45cb76598faf14c9722b0a1aaa7bedbefc7
Bernie Gray <bfgray3@gmail.com>
2023-12-14 17:50:56 -0500

    more 8a

--- 08/a/main.py
+++ 08/a/main.py
@@ -8,18 +8,17 @@ with open(sys.argv[1]) as f:
     for line in f:
         if line == "\n":
             continue
-        if "=" not in line:
-            instructions = line.strip()
-        else:
+        if "=" in line:
             (info,) = re.findall(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)", line)
             nodes[info[0]] = (info[1], info[2])
+        else:
+            instructions = line.strip()
 
 current = "AAA"
 
 for i, direction in enumerate(itertools.cycle(instructions)):
     if current == "ZZZ":
         break
-    else:
-        current = nodes[current][0 if direction == "L" else 1]
+    current = nodes[current][0 if direction == "L" else 1]
 
 print(i)
