a3cf56321e5e247b8025cb9ac4eb54899a2d39fe
Bernie Gray <bfgray3@gmail.com>
2023-12-21 20:11:55 -0500

    use ranges

--- 09/a/main.cpp
+++ 09/a/main.cpp
@@ -2,34 +2,33 @@
 #include <fstream>
 #include <iostream>
 #include <numeric>
+#include <ranges>
 #include <sstream>
 #include <vector>
 
-//template <typename T>
-int get_prediction(const std::vector<int>& v) { // TODO: generalize type
-  std::vector<int> diffs(v.size());
+int get_prediction(const std::ranges::forward_range auto& v) {
+  std::vector<std::ranges::range_value_t<decltype(v)>> diffs(v.size());
   auto last_elts{v.back()};
 
   std::adjacent_difference(std::begin(v), std::end(v), std::begin(diffs));
 
   for (
-    auto num_diffs{v.size() - 1};
+    auto num_diffs{v.size()};
     !std::all_of(
       std::next(std::cbegin(diffs)),
-      std::next(std::cbegin(diffs), num_diffs + 1),
+      std::next(std::cbegin(diffs), num_diffs),
       [](const auto x) { return x == 0; }
     );
     --num_diffs
   ) {
-    last_elts += *std::next(std::cbegin(diffs), num_diffs);
+    last_elts += *std::next(std::cbegin(diffs), num_diffs - 1);
     std::adjacent_difference(
       std::next(std::begin(diffs)),
-      std::next(std::begin(diffs), num_diffs + 1),
+      std::next(std::begin(diffs), num_diffs),
       std::begin(diffs)
     );
   }
   return last_elts;
-
 }
 
 int main(const int, const char** argv) {
